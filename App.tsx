5dcec32fffe837dd1e86b109b2968ba9e83d6fef
Logan Ashamallah <loganash500@gmail.com>
2025-07-20 15:46:24 -0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,9 +1,14 @@
 import "./global.css";
-import React from "react";
+import React, { useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import AppNavigator from "./navigation/AppNavigator";
+import { setupNotifications } from "./services/NotificationService";
 
 export default function App() {
+  useEffect(() => {
+    setupNotifications();
+  }, []);
+
   return (
     <>
       <StatusBar style="auto" />
